a520e212719b47e33e8e9c7712fbd1fe72f3a12a
Ingvar Son <callmejuggernaut@gmail.com>
2019-05-14 00:12:16 +0600

    1021

---
+++ leetcode/1021/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+//string removeOuterParentheses(string s) {
+//    string result;
+//
+//    int i = 0;
+//    while (true) {
+//        int j = 0;
+//
+//        if (s[i] == '(') {
+//            int numOfOpened = 0;
+//            int numOfClosed = 0;
+//
+//            for (j = i + 1; j < s.length(); j++) {
+//                if(s[j] == ')' && numOfOpened == numOfClosed) {
+//                    result += s.substr(i + 1, j - i - 1);
+//
+//                    i = j + 1;
+//                    break;
+//                }
+//
+//                if (s[j] == '(') {
+//                    numOfOpened++;
+//                } else {
+//                    numOfClosed++;
+//                }
+//            }
+//
+//            if (i + 1 > s.length()) {
+//                break;
+//            }
+//        } else {
+//            break;
+//        }
+//    }
+//
+//    return result;
+//}
+
+string removeOuterParentheses(string s) {
+    int count = 0;
+    string result;
+
+    for (char c : s) {
+        if (c == '(') {
+            if (count) {
+                result += c;
+            }
+
+            count++;
+        } else {
+            count--;
+
+            if (count) {
+                result += c;
+            }
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    cout << removeOuterParentheses("(()())(())(()(()))");
+    return 0;
+}
--- leetcode/template/main.cpp
+++ leetcode/template/main.cpp
@@ -1,4 +1,7 @@
-#include "iostream"
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <math>
 
 using namespace std;
 
