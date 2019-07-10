cf07da50bf5be383b3d03b92125a57d3b7805685
Ingvar Son <callmejuggernaut@gmail.com>
2019-07-11 04:28:56 +0600

    905

---
+++ leetcode/scala/src/main/scala/p905.scala
@@ -0,0 +1,11 @@
+object p905 {
+  def sortArrayByParity(A: Array[Int]): Array[Int] = {
+    val b = A.filter(_ % 2 == 0)
+    val c = A.filter(_ % 2 == 1)
+
+    b ++ c
+  }
+
+  val a = sortArrayByParity(Array(0,2,4,6,1))
+  val a1 = 1
+}
